2a24be9b741a06432da0de5cca8938e90acd38c2
zymsbgt <91821082+zymsbgt@users.noreply.github.com>
2022-09-10 18:32:19 +0800

    Stop SpirigatitoOTS from getting pinged

--- script.py
+++ script.py
@@ -13,8 +13,10 @@ async def on_ready():
 @client.event
 async def on_message(message):
     username = str(message.author).split('#')[0]
+    userid = message.author.id
     user_message = str(message.content)
     channel = str(message.channel.name)
+    
     print(f'{username} in #{channel}: {user_message}')
     
     if message.author == client.user:
@@ -22,6 +24,11 @@ async def on_message(message):
     
     if '<@343451476137607179>' in user_message.lower():
         await message.channel.send(f'{username}, please **do not ping** FlashTeens!')
+        #await message.channel.send(f'{message.author.mention}, please **do not ping** FlashTeens!')
+        
+    if userid == 688311805319053336
+        if '<@559210445991444480>' in user_message.lower():
+            await message.channel.send(f'{username}, please **do not ping** SprigatitoOTS!')
     
     if 'Infinite Developer' in username:
         if 'breaking news' in user_message.lower():
