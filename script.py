4f2de3fd23f6694ffeb00d2ebaa7314668709ae8
TG4164 <123239455+TG4164@users.noreply.github.com>
2023-04-04 18:41:03 -0400

    Update script.py

--- script.py
+++ script.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import time
 from dotenv import load_dotenv # new discord bot token library
 
 load_dotenv()
@@ -17,21 +18,25 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    username = str(message.author).split('#')[0]
-    userid = message.author.id
-    user_message = str(message.content)
-    channel = str(message.channel.name)
-    mentioned_users = message.mentions
-    
-    print(f'{username} in #{channel}: {user_message}')
-    
-    if message.author == client.user or message.author.bot:
-        return
-    
-    for user in mentioned_users:
-        if str(user.id) in do_not_ping:
-            userWithoutHashtag = str(user).split('#')[0]
-            await message.channel.send(f'{username}, please **do not ping** {userWithoutHashtag}!')
+    response=False
+    while not response:
+        username = str(message.author).split('#')[0]
+        userid = message.author.id
+        user_message = str(message.content)
+        channel = str(message.channel.name)
+        mentioned_users = message.mentions
+
+        print(f'{username} in #{channel}: {user_message}')
+
+        if message.author == client.user or message.author.bot:
+            return
+
+        for user in mentioned_users:
+            if str(user.id) in do_not_ping:
+                userWithoutHashtag = str(user).split('#')[0]
+                await message.channel.send(f'{username}, please **do not ping** {userWithoutHashtag}!')
+                response = True
+        time.sleep(5)
 
 token = os.getenv('DISCORD_TOKEN')
 client.run(token)
