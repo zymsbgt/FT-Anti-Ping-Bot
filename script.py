d05c281998dae4447bb242c0746969106f53171e
zymsbgt <91821082+zymsbgt@users.noreply.github.com>
2023-04-06 16:39:54 +0800

    Add code to check if FT's message was sent within 30mins

--- script.py
+++ script.py
@@ -2,6 +2,7 @@ import discord
 import os
 import time
 from dotenv import load_dotenv # new discord bot token library
+from datetime import datetime, timedelta
 
 load_dotenv()
 intents = discord.Intents.default()
@@ -18,8 +19,7 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
-    response=False
-    while not response:
+    while True:
         username = str(message.author).split('#')[0]
         userid = message.author.id
         user_message = str(message.content)
@@ -32,9 +32,19 @@ async def on_message(message):
         for user in mentioned_users:
             if str(user.id) in do_not_ping:
                 print(f'{username} in #{channel}: {user_message}')
-                userWithoutHashtag = str(user).split('#')[0]
-                await message.channel.send(f'{username}, please **do not ping** {userWithoutHashtag}!')
-                response = True
+                if message.reference is None: 
+                    userWithoutHashtag = str(user).split('#')[0]
+                    await message.channel.send(f'{username}, please **do not ping** {userWithoutHashtag}!')
+                else: # FT was ping-replied
+                    print('in reply to')
+                    reply_message = await message.channel.fetch_message(message.reference.message_id)
+                    print(f'{reply_message}')
+                    time_diff = datetime.utcnow() - reply_message.created_at
+                    if time_diff < timedelta(minutes=30):
+                        print(f"{message.author} mentioned {userWithoutHashtag} in a reply less than 30 minutes after the original message was sent. Skip sending reminder")
+                    else:
+                        await message.channel.send(f'{username}, please **do not ping** {userWithoutHashtag}!')
+                return
         time.sleep(5)
 
 token = os.getenv('DISCORD_TOKEN')
